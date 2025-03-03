74b47fa35455f376b961c81aac6ead608085263a
Anna Samarina <AnnSamSPb@yandex.ru>
2025-03-04 00:31:44 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -5,35 +5,35 @@ const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
 // Функция создания карточки
-function createCard(cardData, deleteCardCallback) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+function createCardElement(cardData, deleteCard) {
+  const newCard = cardTemplate.querySelector(".card").cloneNode(true);
 
-  const cardImage = cardElement.querySelector(".card__image");
-  const cardTitle = cardElement.querySelector(".card__title");
-  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = newCard.querySelector(".card__image");
+  const cardTitle = newCard.querySelector(".card__title");
+  const deleteButton = newCard.querySelector(".card__delete-button");
 
   cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
+  cardImage.alt = `Фото случайного места в ${cardData.name}`;
   cardTitle.textContent = cardData.name;
 
   // Обработчик для удаления карточки
-  deleteButton.addEventListener("click", deleteCardCallback);
+  deleteButton.addEventListener("click", deleteCard);
 
-  return cardElement;
+  return newCard;
 }
 
 // Функция удаления карточки
 function deleteCard(evt) {
-  const cardElement = evt.target.closest(".card");
-  cardElement.remove();
+  const targetCard = evt.target.closest(".card");
+  targetCard.remove();
 }
 
 // Вывести карточки на страницу
-function renderCard(cardData, container) {
-  const cardElement = createCard(cardData, deleteCard);
-  container.appendChild(cardElement);
+function renderCardElement(cardData, container) {
+  const cardElement = createCardElement(cardData, deleteCard);
+  container.append(cardElement);
 }
 
-initialCards.forEach((cardData) => {
-  renderCard(cardData, placesList);
+initialCards.forEach(cardData => {
+  renderCardElement(cardData, placesList);
 });
