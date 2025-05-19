65b19249bd3e241910e3292b73390c361bd0d190
Anna Samarina <AnnSamSPb@yandex.ru>
2025-05-19 12:26:26 +0300

    sprint7

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,25 @@ module.exports = {
     rules: [{
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: '/node_modules/',
-        // use: {
-        //   loader: 'babel-loader', // Транспиляция JS (если нужна)
-        //   options: {
-        //     presets: ['@babel/preset-env'],
-        //   },
-        // },  
+        exclude: '/node_modules/', 
+      },
+      // {
+      //   test: /\.(jpg|png|svg|gif|woff(2)?|eot|ttf|otf)$/,
+      //   type: 'asset/resource',
+      // },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]',
+        }
       },
       {
-        test: /\.(jpg|png|svg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]',
+        }
       },
       {
         test: /\.css$/,
